11c8c2ed24024db8bd5c84b06b26ee25d7ab5bf3
ebiner3 <51997906+ebiner3@users.noreply.github.com>
2021-05-18 16:25:13 +0300

    Added more workload options to vssdl.bat
    Also removed the unnecessary lang en-US option from the vssintall.bat

--- VSISO/vssdl.bat
+++ VSISO/vssdl.bat
@@ -1,5 +1,22 @@
 rem vs_professional.exe --layout C:\VSProLayout3 --lang en-US --add Microsoft.VisualStudio.Workload.Python --includeRecommended --includeOptional
 rem vs_professional.exe --layout C:\VSProLayout3 --lang en-US --add Microsoft.VisualStudio.Workload.Data --includeRecommended --includeOptional
-vs_professional.exe --layout C:\VSProLayout3 --lang en-US --add Microsoft.VisualStudio.Workload.NativeDesktop --includeRecommended --includeOptional
+rem vs_professional.exe --layout C:\VSProLayout3 --lang en-US --add Microsoft.VisualStudio.Workload.NativeDesktop --includeRecommended --includeOptional
 rem vs_professional.exe --layout C:\VSProLayout3 --lang en-US --add Microsoft.VisualStudio.Workload.NativeGame --includeRecommended --includeOptional
 rem vs_professional.exe --layout C:\VSProLayout3 --lang en-US --add Microsoft.VisualStudio.Workload.ManagedDesktop --includeRecommended --includeOptional
+
+rem vs_professional.exe --layout C:\VSProLayout3 --lang en-US --add Component.GitHub.VisualStudio --includeRecommended --includeOptional
+rem vs_professional.exe --layout C:\VSProLayout3 --lang en-US --add Microsoft.Component.HelpViewer --includeRecommended --includeOptional
+rem vs_professional.exe --layout C:\VSProLayout3 --lang en-US --add Microsoft.VisualStudio.Component.ClassDesigner --includeRecommended --includeOptional
+rem vs_professional.exe --layout C:\VSProLayout3 --lang en-US --add Microsoft.VisualStudio.Component.Git --includeRecommended --includeOptional
+rem vs_professional.exe --layout C:\VSProLayout3 --lang en-US --add Microsoft.VisualStudio.Component.GraphDocument --includeRecommended --includeOptional
+rem vs_professional.exe --layout C:\VSProLayout3 --lang en-US --add Microsoft.VisualStudio.Component.LinqToSql --includeRecommended --includeOptional
+
+
+rem vs_professional.exe --layout C:\VSProLayout3 --lang en-US --add Microsoft.VisualStudio.Workload.Azure --includeRecommended --includeOptional
+rem vs_professional.exe --layout C:\VSProLayout3 --lang en-US --add Microsoft.VisualStudio.Workload.ManagedGame --includeRecommended --includeOptional
+rem vs_professional.exe --layout C:\VSProLayout3 --lang en-US --add Microsoft.VisualStudio.Workload.Node --includeRecommended --includeOptional
+rem vs_professional.exe --layout C:\VSProLayout3 --lang en-US --add Microsoft.VisualStudio.Workload.NetWeb --includeRecommended --includeOptional
+rem vs_professional.exe --layout C:\VSProLayout3 --lang en-US --add Microsoft.VisualStudio.Workload.Universal --includeRecommended --includeOptional
+rem vs_professional.exe --layout C:\VSProLayout3 --lang en-US --add Microsoft.VisualStudio.Workload.NetCrossPlat --includeRecommended --includeOptional
+rem vs_professional.exe --layout C:\VSProLayout3 --lang en-US --add Microsoft.VisualStudio.Workload.NativeMobile --includeRecommended --includeOptional
+
---
+++ VSISO/vssinstall_extended.bat
@@ -0,0 +1,24 @@
+C:\VSProLayout3\vs_professional.exe --noweb  ^
+   --add Microsoft.VisualStudio.Workload.Python   ^
+   --add Microsoft.VisualStudio.Workload.Data   ^
+   --add Microsoft.VisualStudio.Workload.NativeDesktop   ^
+   --add Microsoft.VisualStudio.Workload.NativeGame   ^
+   --add Microsoft.VisualStudio.Workload.ManagedDesktop   ^
+
+   --add Component.GitHub.VisualStudio   ^
+   --add Microsoft.Component.HelpViewer   ^
+   --add Microsoft.VisualStudio.Component.ClassDesigner   ^
+   --add Microsoft.VisualStudio.Component.Git   ^
+   --add Microsoft.VisualStudio.Component.GraphDocument   ^
+   --add Microsoft.VisualStudio.Component.LinqToSql   ^
+
+
+   --add Microsoft.VisualStudio.Workload.Azure   ^
+   --add Microsoft.VisualStudio.Workload.ManagedGame   ^
+   --add Microsoft.VisualStudio.Workload.Node   ^
+   --add Microsoft.VisualStudio.Workload.NetWeb   ^
+   --add Microsoft.VisualStudio.Workload.Universal   ^
+   --add Microsoft.VisualStudio.Workload.NetCrossPlat   ^
+   --add Microsoft.VisualStudio.Workload.NativeMobile   ^
+    --includeRecommended --includeOptional
+
