43455bd4f8aa3f16ae989799fd0bc10caf067614
ebiner3 <51997906+ebiner3@users.noreply.github.com>
2021-05-21 10:35:25 +0300

    Added files for 2 volume set
    One volume excedded 10GB, needed to break down

---
+++ VSISO_2_Volume_Set/vssdl.bat
@@ -0,0 +1,25 @@
+REM VOLUME 1
+rem vs_professional.exe --layout C:\VsProLayoutVol01 --lang en-US --add Microsoft.VisualStudio.Workload.Python --includeRecommended --includeOptional
+rem vs_professional.exe --layout C:\VsProLayoutVol01 --lang en-US --add Microsoft.VisualStudio.Workload.Data --includeRecommended --includeOptional
+rem vs_professional.exe --layout C:\VsProLayoutVol01 --lang en-US --add Microsoft.VisualStudio.Workload.NativeDesktop --includeRecommended --includeOptional
+rem vs_professional.exe --layout C:\VsProLayoutVol01 --lang en-US --add Microsoft.VisualStudio.Workload.NativeGame --includeRecommended --includeOptional
+rem vs_professional.exe --layout C:\VsProLayoutVol01 --lang en-US --add Microsoft.VisualStudio.Workload.ManagedDesktop --includeRecommended --includeOptional
+
+REM VOLUME 2
+rem vs_professional.exe --layout C:\VsProLayoutVol02 --lang en-US --add Microsoft.VisualStudio.Workload.NetWeb --includeRecommended --includeOptional
+rem vs_professional.exe --layout C:\VsProLayoutVol02 --lang en-US --add Microsoft.VisualStudio.Workload.ManagedGame --includeRecommended --includeOptional
+rem vs_professional.exe --layout C:\VsProLayoutVol02 --lang en-US --add Microsoft.VisualStudio.Workload.Node --includeRecommended --includeOptional
+rem vs_professional.exe --layout C:\VsProLayoutVol02 --lang en-US --add Microsoft.VisualStudio.Workload.Universal --includeRecommended --includeOptional
+rem vs_professional.exe --layout C:\VsProLayoutVol02 --lang en-US --add Microsoft.VisualStudio.Workload.Azure --includeRecommended --includeOptional
+rem vs_professional.exe --layout C:\VsProLayoutVol02 --lang en-US --add Microsoft.VisualStudio.Workload.NetCrossPlat --includeRecommended --includeOptional
+rem vs_professional.exe --layout C:\VsProLayoutVol02 --lang en-US --add Microsoft.VisualStudio.Workload.NativeMobile --includeRecommended --includeOptional
+
+
+rem vs_professional.exe --layout C:\VsProLayoutVol02 --lang en-US --add Component.GitHub.VisualStudio --includeRecommended --includeOptional
+rem vs_professional.exe --layout C:\VsProLayoutVol02 --lang en-US --add Microsoft.Component.HelpViewer --includeRecommended --includeOptional
+rem vs_professional.exe --layout C:\VsProLayoutVol02 --lang en-US --add Microsoft.VisualStudio.Component.ClassDesigner --includeRecommended --includeOptional
+rem vs_professional.exe --layout C:\VsProLayoutVol02 --lang en-US --add Microsoft.VisualStudio.Component.Git --includeRecommended --includeOptional
+rem vs_professional.exe --layout C:\VsProLayoutVol02 --lang en-US --add Microsoft.VisualStudio.Component.GraphDocument --includeRecommended --includeOptional
+rem vs_professional.exe --layout C:\VsProLayoutVol02 --lang en-US --add Microsoft.VisualStudio.Component.LinqToSql --includeRecommended --includeOptional
+
+
